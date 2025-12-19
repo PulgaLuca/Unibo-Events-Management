b64a76bfbe663b902549066334a0953ca28c06eb
Marco Casali <marco.casali13@studio.unibo.it>
2025-12-20 00:51:47 +0100

    implemented request lifecycle through router in index

--- public/index.php
+++ public/index.php
@@ -1,4 +1,25 @@
 <?php
 
+declare(strict_types=1);
 
-?>
+use App\Infrastructure\Container\ContainerFactory;
+use App\Infrastructure\Http\Request;
+use App\Infrastructure\Http\Router;
+use Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+Dotenv::createImmutable(__DIR__ . '/..')->safeLoad();
+
+session_start();
+
+try {
+	$container = ContainerFactory::build();
+	$request = Request::fromGlobals();
+	$response = $container->get(Router::class)->dispatch($request);
+	$response->send();
+} catch (Throwable $exception) {
+	http_response_code(500);
+	header('Content-Type: application/json');
+	echo json_encode(['error' => 'Internal server error', 'detail' => $exception->getMessage()]);
+}

39472de563bed3e66b7a2ec0bd8f7c9fbd960610
Marco Casali <marco.casali13@studio.unibo.it>
2025-12-20 00:48:22 +0100

    Created DI container factory, Http request and reponse, Router models

---
+++ src/Infrastructure/Container/ContainerFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Container;
+
+use DI\ContainerBuilder;
+use Psr\Container\ContainerInterface;
+
+class ContainerFactory
+{
+    public static function build(): ContainerInterface
+    {
+        $builder = new ContainerBuilder();
+        $definitions = require __DIR__ . '/../../config/container.php';
+        $builder->addDefinitions($definitions);
+
+        return $builder->build();
+    }
+}
---
+++ src/Infrastructure/Http/Response.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Http;
+
+class Response
+{
+    private int $statusCode;
+    private array $headers;
+    private string $body;
+
+    public function __construct(string $body = '', int $statusCode = 200, array $headers = [])
+    {
+        $this->statusCode = $statusCode;
+        $this->headers = $headers;
+        $this->body = $body;
+    }
+
+    public static function json(array $data, int $statusCode = 200): self
+    {
+        return new self(json_encode($data, JSON_UNESCAPED_UNICODE), $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public static function text(string $text, int $statusCode = 200): self
+    {
+        return new self($text, $statusCode, ['Content-Type' => 'text/plain; charset=utf-8']);
+    }
+
+    public static function html(string $html, int $statusCode = 200): self
+    {
+        return new self($html, $statusCode, ['Content-Type' => 'text/html; charset=utf-8']);
+    }
+
+    public function send(): void
+    {
+        http_response_code($this->statusCode);
+        foreach ($this->headers as $name => $value) {
+            header("{$name} : {$value}");
+        }
+        echo $this->body;
+    }
+}
